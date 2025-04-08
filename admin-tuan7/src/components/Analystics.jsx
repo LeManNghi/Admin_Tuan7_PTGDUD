8741d68a35519686f07f9d3d5442a1d9d63f6962
ManNghi <22652921.nghi@student.iuh.edu.vn>
2025-04-08 23:48:26 +0700

    5.React Router + 7.Single Page Application

---
+++ admin-tuan7/src/components/Analystics.jsx
@@ -0,0 +1,7 @@
+export default function Analystics() {
+    return(
+        <div>
+            <h1>Analystics</h1>
+        </div>
+    )
+}
---
+++ admin-tuan7/src/components/DashboardHeader.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './DashboardHeader.css';
+export default function DashboardHeader() {
+    return (
+        <div className="dashboard-header">
+            <h2>Dashboard</h2>
+            <div className="header-actions">
+                <img className='search' src="./src/images/Search.png" alt="" />
+                <input type="text" placeholder="Search" />
+                <img src="./src/images/Bell.png" alt="" />
+                <img src="./src/images/Question.png" alt="" />
+                <img src="./src/images/Avatar.png" alt="" />
+            </div>
+        </div>
+    )
+}
