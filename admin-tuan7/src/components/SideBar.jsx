c2b7fd379e9fbb9f887718ee827d237a0868629e
ManNghi <22652921.nghi@student.iuh.edu.vn>
2025-04-09 11:49:48 +0700

    NavLink React Router + Single Page Application

--- admin-tuan7/src/components/SideBar.jsx
+++ admin-tuan7/src/components/SideBar.jsx
@@ -1,29 +1,35 @@
-import { Link } from 'react-router-dom'
+import { NavLink, useMatch, useResolvedPath } from "react-router-dom";
 import './SideBar.css'
 
 const SideBar = () => {
+    const navLinkStyle = ({ isActive }) => ({
+        color: isActive ? '#fff' : '',
+        backgroundColor: isActive ? '#f44b86' : ''
+    })
+
+    const CustomLink = ({ children, to, img, ...props }) => {
+        const resolved = useResolvedPath(to)
+        const match = useMatch({ path: resolved.pathname, end: true })
+        return (
+            <li className="nav-link" style={match ? { color: '#fff', backgroundColor: '#f44b86' } : {}}>
+                <NavLink to={to} {...props}>
+                    <img src={img} alt="" />
+                    {children}
+                </NavLink>
+            </li>
+        )
+    }
+
   return (
     <div>
         <img src="./src/images/Logo.png" alt="" />
         <ul>
-            <li>
-                <Link to='/' className='nav-link'>Dashboard</Link>
-            </li>
-            <li>
-                <Link to='/projects' className='nav-link'>Projects</Link>
-            </li>
-            <li>
-                <Link to='/teams' className='nav-link'>Teams</Link>
-            </li>
-            <li>
-                <Link to='/analystics' className='nav-link'>Analystics</Link>
-            </li>
-            <li>
-                <Link to='/messages' className='nav-link'>Messages</Link>
-            </li>
-            <li>
-                <Link to='/integrations' className='nav-link'>Integrations</Link>
-            </li>
+            <CustomLink to='/' img='./src/images/Squares four 1.png'>Dashboard</CustomLink>
+            <CustomLink to='/projects' img='./src/images/Folder.png'>Projects</CustomLink>
+            <CustomLink to='/teams' img='./src/images/Groups.png'>Teams</CustomLink>
+            <CustomLink to='/analystics' img='./src/images/Pie chart.png'>Analystics</CustomLink>
+            <CustomLink to='/messages' img='./src/images/Chat.png'>Messages</CustomLink>
+            <CustomLink to='/integrations' img='./src/images/Code.png'>Integrations</CustomLink>
         </ul>
         <div className='sidebar-footer'>
             <img src="./src/images/Group.png" alt="" />
