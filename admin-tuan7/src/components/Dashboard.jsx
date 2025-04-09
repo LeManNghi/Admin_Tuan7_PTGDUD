15b0946877bd129835b0e3ef228f25eb6af97cc1
ManNghi <22652921.nghi@student.iuh.edu.vn>
2025-04-09 14:01:02 +0700

    8.Edit Model

--- admin-tuan7/src/components/Dashboard.jsx
+++ admin-tuan7/src/components/Dashboard.jsx
@@ -2,77 +2,104 @@ import { useState } from 'react'
 import { useEffect } from 'react'
 import DataTable from 'react-data-table-component'
 import OverviewItem from './OverViewItem';
+import EditModal from './EditModel';
 
-
-const columns = [
-  {
-    cell: row => (
-      <input type="checkbox" onChange={() => {}} />
-    ),
-    maxWidth: "50px",
-  },
-  {
-    name: 'CUSTOMER NAME',
-    selector: row => row.customer_name,
-    cell: row => (
-      <div className='customer-name'>
-        <img src={row.avatar} alt=''/>
-        <span>{row.customer_name}</span>
-      </div>
-    ),
-    grow: 2,
-    sortable: true,
-  },
-  {
-    name: 'COMPANY',
-    selector: row => row.company,
-    sortable: true,
-  },
-  {
-    name: 'ORDER VALUE',
-    selector: row => row.order_value,
-    sortable: true,
-  },
-  {
-    name: 'ORDER DATE',
-    selector: row => row.order_date,
-    sortable: true,
-  },
-  {
-    name: 'STATUS',
-    selector: row => row.status,
-    cell: row => (
-      <span className={row.status}>{row.status}</span>
-    ),
-    sortable: true,
-  },
-  {
-    cell: row => (
-      <button><img src="./src/images/create.png" alt="" /></button>
-    ),
-    right: true,
-  },
-];
 
 export default function Dashboard() {
 
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [editingUser, setEditingUser] = useState(null);
+
+  const handleEdit = (user) => {
+    setEditingUser(user);
+    setIsEditModalOpen(true);
+  };
+
+  const handleSave = async (updatedUser) => {
+    try {
+      // Gửi PUT request tới API
+      await fetch(`https://67f3bab0cbef97f40d2bd34b.mockapi.io/detailed_report/report/${updatedUser.id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(updatedUser),
+      });
+  
+      // Cập nhật lại dữ liệu trong local state
+      setReport(prev => prev.map(item => item.id === updatedUser.id ? updatedUser : item));
+      setIsEditModalOpen(false);
+    } catch (error) {
+      console.error('Update failed:', error);
+    }
+  };
+
+  const columns = [
+    {
+      cell: row => (
+        <input type="checkbox" onChange={() => {}} />
+      ),
+      width: '50px',
+    },
+    {
+      name: 'CUSTOMER NAME',
+      selector: row => row.customer_name,
+      cell: row => (
+        <div className='customer-name'>
+          <img src={row.avatar} alt=''/>
+          <span>{row.customer_name}</span>
+        </div>
+      ),
+      grow: 2,
+      sortable: true,
+    },
+    {
+      name: 'COMPANY',
+      selector: row => row.company,
+      sortable: true,
+    },
+    {
+      name: 'ORDER VALUE',
+      selector: row => row.order_value,
+      sortable: true,
+    },
+    {
+      name: 'ORDER DATE',
+      selector: row => row.order_date,
+      sortable: true,
+    },
+    {
+      name: 'STATUS',
+      selector: row => row.status,
+      cell: row => (
+        <span className={row.status}>{row.status}</span>
+      ),
+      sortable: true,
+    },
+    {
+      cell: row => (
+        <button style={{backgroundColor:'#ffff'}} onClick={() => handleEdit(row)}><img src="./src/images/create.png" alt="" /></button>
+      ),
+      right: true,
+    },
+  ];
   const [report, setReport] = useState([]);
   const [overview, setOverview] = useState([]);
 
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://67f3bab0cbef97f40d2bd34b.mockapi.io/detailed_report');
+      const data = await response.json();
+
+      setReport(data[0].report);
+      setOverview(data[0].overview);
+    } catch (error) {
+      console.error("Erro:", error);
+    }
+  };
+
   useEffect(() => {
-    const fetchReport = async () => {
-      try {
-        const response = await fetch('https://67f3bab0cbef97f40d2bd34b.mockapi.io/detailed_report');
-        const data = await response.json();
-
-        setReport(data[0].report);
-        setOverview(data[0].overview);
-      } catch (error) {
-        console.error("Erro:", error);
-      }
-    };
-
-    fetchReport();
+    fetchData();
   }, []);
 
   const [currentPage, setCurrentPage] = useState(1);
@@ -100,6 +127,7 @@ export default function Dashboard() {
             {overview.map((detail) => {
                 return (
                   <OverviewItem 
+                    key={detail.id}
                     label={detail.label} 
                     value={detail.value} 
                     percentage={detail.percentage} 
@@ -133,9 +161,15 @@ export default function Dashboard() {
             onChangePage={handlePageChange}
           />
           <div className='result'>{report.length} results</div>
-          
         </div>
       </div>
+      {isEditModalOpen && (
+        <EditModal 
+        user={editingUser}
+        onClose={() => setIsEditModalOpen(false)}
+        onSave={handleSave}
+      />
+      )}
     </>
   )
 }
